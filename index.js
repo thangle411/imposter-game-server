e24ea3af023676c100c6410bb3d8a10edc3a1c38
Thang Le <thangleee228@gmail.com>
2026-01-07 23:04:38 -0800

    test vercel with boilerplate with app.listen

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello from Express on Vercel!' });
 });
 
-// Export the Express app
-export default app;
+app.listen(3000, () => {
+    console.log(`Server is running on port ${port}`);
+});
