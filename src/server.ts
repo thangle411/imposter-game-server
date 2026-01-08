332b2694c6bd818f764a645a50f0b55efda6c8ef
Thang Le <thangleee228@gmail.com>
2026-01-07 22:51:57 -0800

    test vercel

--- src/server.ts
+++ src/server.ts
@@ -29,8 +29,8 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
 
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
 export default app
